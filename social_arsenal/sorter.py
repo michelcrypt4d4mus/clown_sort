4f180ead7b89ffdb67bb45b22a19769848a1e72e
ashariyar <ashariyar@users.noreply.github.com>
2023-02-19 01:32:53 -0500

    Cruft

--- social_arsenal/sorter.py
+++ social_arsenal/sorter.py
@@ -1,16 +1,15 @@
 """
 Sort images based on the extracted contents.
 """
-import re
 from os import path
 from pathlib import Path
 from typing import List, Union
 
-from social_arsenal.config import MAC_SCREENSHOT_REGEX, Config
+from social_arsenal.config import Config
 from social_arsenal.files.image_file import ImageFile
 from social_arsenal.files.pdf_file import PdfFile
 from social_arsenal.files.sortable_file import SortableFile
-from social_arsenal.util.filesystem_helper import files_in_dir, is_image, is_pdf, is_sortable
+from social_arsenal.util.filesystem_helper import files_in_dir, is_image, is_pdf
 
 
 def screenshot_paths() -> List[SortableFile]:
@@ -23,16 +22,6 @@ def screenshot_paths() -> List[SortableFile]:
     return sorted(screenshots, key=lambda f: f.basename)
 
 
-# def sortable_non_screenshot_paths() -> List[SortableFile]:
-#     """Returns a list of ImageFiles for all the screenshots to be sorted."""
-#     files = [
-#         _build_sortable_file(f) for f in files_in_dir(Config.screenshots_dir)
-#         if is_sortable(f) and not path.basename(f).startswith('Screen Shot')
-#     ]
-
-#     return sorted(files, key=lambda f: str(f))
-
-
 def _build_sortable_file(file_path: Union[str, Path]) -> SortableFile:
     if is_image(file_path):
         return ImageFile(file_path)
@@ -40,7 +29,3 @@ def _build_sortable_file(file_path: Union[str, Path]) -> SortableFile:
         return PdfFile(file_path)
     else:
         return SortableFile(file_path)
-
-
-def _is_screenshot(file_path: Union[str, Path]) -> bool:
-    return SCREENSHOT_REGEX.search(str(file_path)) is not None
