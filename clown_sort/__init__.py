ca731d44ab265082eea929ea1d272df71d09fdc1
ashariyar <ashariyar@users.noreply.github.com>
2023-02-21 18:57:36 -0500

    Remove sorter.py

--- clown_sort/__init__.py
+++ clown_sort/__init__.py
@@ -1,12 +1,16 @@
 from os import environ, getcwd, path
 from pathlib import Path
+from typing import List, Union
 
 from dotenv import load_dotenv
 
 from clown_sort.config import Config
-from clown_sort.sorter import screenshot_paths
+from clown_sort.files.image_file import ImageFile
+from clown_sort.files.pdf_file import PdfFile
+from clown_sort.files.sortable_file import SortableFile
 from clown_sort.util.argument_parser import parse_arguments
-from clown_sort.util.filesystem_helper import set_timestamp_based_on_screenshot_filename
+from clown_sort.util.filesystem_helper import (files_in_dir, is_image, is_pdf,
+     set_timestamp_based_on_screenshot_filename)
 
 # load_dotenv() should be called as soon as possible (before parsing local classes) but not for pytest
 if not environ.get('INVOKED_BY_PYTEST', False):
@@ -28,3 +32,22 @@ def set_screenshot_timestamps():
 
     for image in screenshot_paths(Config.screenshots_dir):
         set_timestamp_based_on_screenshot_filename(image.file_path)
+
+
+def screenshot_paths(dir: Path) -> List[SortableFile]:
+    """Returns a list of ImageFiles for all the screenshots to be sorted."""
+    screenshots = [
+        _build_sortable_file(f) for f in files_in_dir(Config.screenshots_dir)
+        if not Config.screenshots_only or Config.filename_regex.match(path.basename(f))
+    ]
+
+    return sorted(screenshots, key=lambda f: f.basename)
+
+
+def _build_sortable_file(file_path: Union[str, Path]) -> SortableFile:
+    if is_image(file_path):
+        return ImageFile(file_path)
+    elif is_pdf(file_path):
+        return PdfFile(file_path)
+    else:
+        return SortableFile(file_path)
